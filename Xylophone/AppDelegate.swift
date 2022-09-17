9f2204ec6357cf8953758aeb3c526a4bbf78e4cf
abdallah <abdullah.wafik1@gmail.com>
2022-09-17 16:45:29 +0200

    finished the project

--- Xylophone/AppDelegate.swift
+++ Xylophone/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+//        let audioSession = AVAudioSession.sharedInstance()
+//         do {
+//            try audioSession.setCategory(.playback)
+//             try audioSession.setActive(true)
+//         } catch {
+//             print("Setting category to AVAudioSessionCategoryPlayback failed.")
+//         }
         // Override point for customization after application launch.
         return true
     }
--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -1,21 +1,49 @@
-//
 //  ViewController.swift
 //  Xylophone
-//
 //  Created by Angela Yu on 28/06/2019.
 //  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
 
+    var player : AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
+    @IBAction func keyPressed(_ sender: UIButton) {
+        //get the name of the button
+        guard let buttonName =  sender.titleLabel?.text else {return}
 
+            playSound(buttonName: buttonName)
+        changeButtonOpicty(button: sender)
+    }
+        
+        //get name of the button ,then play sound from bundle
+    func playSound(buttonName : String){
+        
+        guard  let url = Bundle.main.url(forResource: buttonName, withExtension: "wav") else {return}
+       do {
+            //try AVAudioSession.sharedInstance().setCategory(.playback, mode:  .default)
+           // try AVAudioSession.sharedInstance().setActive(true)
+           //player = try AVAudioPlayer(contentsOf: path)
+            player = try AVAudioPlayer(contentsOf: url)
+            //player.prepareToPlay()
+           player?.play()
+       }
+        catch { print("er00000ror") }
+    }
+        
+    /*change the opacity of button to 0.5 then restore it to 1 after
+    0.2 second
+     */
+   func changeButtonOpicty(button : UIButton){
+       button.alpha = 0.5
+    DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+        button.alpha = 1
+        }
+    }
 }
 
